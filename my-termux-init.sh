e9857df789c2a64d4794dbf9d8730fb9636fca1a
ZainIoT <38690228+ZainIoT@users.noreply.github.com>
2019-09-28 18:09:20 +0800

    int

--- my-termux-init.sh
+++ my-termux-init.sh
@@ -21,10 +21,17 @@ unset changeSources
 echo "使用扩展键盘？"
 read -p "确认 [Y/y]，取消 [N/n]" changeKey
 if [ $changeKey = 'Y' ] || [ $changeKey = 'y' ] ;then
-  echo "确认"
-  echo "extra-keys = [['ESC','TAB','{','}','|','HOME','UP','END','PGUP'],['CTRL','ALT','<','>','-','LEFT','DOWN','RIGHT','PGDN']]" > $HOME/.termux/termux.properties
+    echo "ok"
+    if [ ! -e "$HOME/.termux" ] ;then
+        mkdir "$HOME/.termux"
+    fi
+    if [ ! -e "$HOME/.termux/termux.properties" ] ;then
+        touch "$HOME/.termux/termux.properties"
+        echo ok
+    fi
+    echo "extra-keys = [['ESC','TAB','{','}','|','HOME','UP','END','PGUP'],['CTRL','ALT','<','>','-','LEFT','DOWN','RIGHT','PGDN']]" > $HOME/.termux/termux.properties
 else
-  echo "已取消"
+    echo "cancel"
 fi
 unset changeKey
 
@@ -34,7 +41,8 @@ read -p "确认 [Y/y]，取消 [N/n]" changeBoot
 case ${changeBoot} in
     Y | y)
         echo "ok"
-        echo -e "# The main termux repository:\n# deb https://dl.bintray.com/termux/termux-packages-24 stable main\n\n# The termux repository mirror from TUNA:\ndeb https://mirrors.tuna.tsinghua.edu.cn/termux stable main" > $PREFIX/etc/apt/sources.list
+        curl -O https://raw.githubusercontent.com/ZainIoT/Termux/master/motd
+        mv motd $PREFIX/etc/motd
         ;;
     N | n)
         echo "cancel"
